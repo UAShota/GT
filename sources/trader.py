621a8fa9dbfb37cef139b5ca31f7eb224d1c0e0b
UAShota <mdv.uashota@gmail.com>
2021-08-11 10:11:38 +0300

    hub

--- sources/trader.py
+++ sources/trader.py
@@ -1,36 +1,15 @@
 ﻿"""
 Trade bot module
 """
-
+import datetime
 import json
+import random
 import re
+import threading
 import time
-import traceback
 import urllib.parse
-from threading import Thread
-from typing import Optional
 
 import requests
-from vk_api import VkApi
-from vk_api.longpoll import Event, VkLongPoll, VkEventType
-
-
-class TradeSlot:
-    """ Simple slot description """
-    name: str = ""
-    short: str = ""
-    cost: int = 0
-    code: int = 0
-
-    def __repr__(self):
-        return [self.name, self.cost, self.short, self.code].__str__()
-
-
-class TradeLot:
-    """ Simple lot description """
-    cost: int = 0
-    count: int = 0
-    lotnum: int = 0
 
 
 class TraderApi:
@@ -105,15 +84,17 @@ class TraderApi:
         # Completed array
         return tmp_params
 
-    def queryLots(self, itemid: int):
+    def queryLots(self, itemid: int, itemname: str):
         """ Translate item params """
         tmp_referer = self.API_URL % (self.ACT_TYPE_ITEM % itemid, self.bagid)
         tmp_response = requests.get(self.API_URL % (self.ACT_TYPE_ITEM % itemid, self.bagid), headers=self.buildHeaders(0, tmp_referer))
         if not tmp_response.ok:
+            print("%s failed item %d query" % (self.bagid, itemid))
             return False
         # Item defense
-        tmp_params = re.search(r"window.pv624 = ({.+})", tmp_response.text)
+        tmp_params = re.search(r"window.pv\d+ = ({.+})", tmp_response.text)
         if not tmp_params:
+            print("%s failed item %d params" % (self.bagid, itemid))
             return False
         tmp_params = json.loads(tmp_params[1])
         tmp_params = {
@@ -132,238 +113,96 @@ class TraderApi:
         tmp_reitems = self.relot.findall(tmp_response.text)
         tmp_lots = []
         for tmp_reitem in tmp_reitems:
-            tmp_lot = TradeLot()
-            tmp_lot.count = int(tmp_reitem[0])
-            tmp_lot.cost = int(tmp_reitem[2])
-            tmp_lot.lotnum = int(tmp_reitem[3])
-            tmp_lots.append(tmp_lot)
-        return tmp_lots
+            tmp_lots.append([int(tmp_reitem[0]), int(tmp_reitem[2]), int(tmp_reitem[3])])
+        return [datetime.datetime.now().timestamp(), tmp_lots, itemname]
 
 
 class Trader:
     """ Движок обработки торговли """
 
-    # Идентификатор торгового бота
-    TRADE_BOT_ID = -183040898
-
-    # Идентификатор игрового бота
-    GAME_BOT_ID = -182985865
-
     # Имя файла данных
     DATA_NAME = "data.txt"
+    # Export filename
+    EXPORT_NAME = "../../var/www/html/export.txt"
+    # Export jsname
+    EXPORT_NAME_JS = "../../var/www/html/export.js"
 
-    def __init__(self, token: str, bagid: str, ownerid: int, ownertoken: str = ""):
+    def __init__(self, bagids: []):
         """ Конструктор """
-        if ownertoken:
-            self.ownersession = VkApi(token=ownertoken)
-            self.ownerlongpoll = VkLongPoll(self.ownersession)
-            self.threadlpowner = Thread(target=self.lpownerthread)
-        else:
-            self.ownersession = None
-            self.ownerlongpoll = None
-            self.threadlpowner = None
-        self.ownerid = ownerid
-        self.tradersession = VkApi(token=token)
-        self.traderlongpoll = VkLongPoll(self.tradersession)
-        self.traderapi = TraderApi(bagid)
         self.lots = []
+        self.data = {}
         self.lotkey = 0
-        self.event: Optional[Event] = None
-        self.reg_all = self.traderapi.compile(r"^скупка кажи")
-        self.reg_set = self.traderapi.compile(r"^скупка (.+?) (\d+)")
-        self.reg_accept = self.traderapi.compile(r"^⚖.+Вы успешно приобрели с аукциона предмет (\d+)\*(.+) -")
-        self.threadbuy = Thread(target=self.buythread)
-        self.threadlptrader = Thread(target=self.lptraderthread)
+        self.bagids = bagids
+        self.locker = threading.Lock()
         self.load()
-        self.run()
+        self.save()
 
-    def run(self):
-        """ Запуск жизненного цикла """
-        print("> Ready")
-        if self.threadbuy:
-            self.threadbuy.start()
-        if self.threadlptrader:
-            self.threadlptrader.start()
-        if self.threadlpowner:
-            self.threadlpowner.start()
-
-    def buythread(self):
-        """ Покупка в потоке """
-        while True:
-            try:
-                self.buy()
-            except Exception as e:
-                print("Buy failed %s %s" % (e, traceback.format_exc().replace("\n", " ")))
-                time.sleep(3)
-
-    def lptraderthread(self):
-        """ Trader long poll thread """
-        while True:
-            try:
-                for self.event in self.traderlongpoll.check():
-                    if self.event.type == VkEventType.MESSAGE_NEW:
-                        self.checktrader()
-                        if not self.ownerlongpoll:
-                            self.checkowner()
-            except Exception as e:
-                print("Read trade failed %s %s" % (e, traceback.format_exc().replace("\n", " ")))
-                time.sleep(3)
-
-    def lpownerthread(self):
-        """ Trader long poll thread """
-        while True:
-            try:
-                for self.event in self.ownerlongpoll.check():
-                    if self.event.type == VkEventType.MESSAGE_NEW:
-                        self.checkowner()
-            except Exception as e:
-                print("Read owner failed %s %s" % (e, traceback.format_exc().replace("\n", " ")))
-                time.sleep(3)
+        for tmp_bagid in bagids:
+            threading.Thread(target=self.run, args=(TraderApi(tmp_bagid),)).start()
+            time.sleep(random.randint(3, 15))
 
     def load(self):
         """ Загрузка БД из файла """
         with open(self.DATA_NAME, 'r', encoding='utf-8') as tmpFile:
-            tmp_items = json.load(tmpFile)
-            for tmp_item in tmp_items:
-                tmp_slot = TradeSlot()
-                tmp_slot.name = tmp_item[0]
-                tmp_slot.cost = tmp_item[1]
-                tmp_slot.short = tmp_item[2]
-                tmp_slot.code = tmp_item[3]
-                self.lots.append(tmp_slot)
+            self.lots = json.load(tmpFile)
+        with open(self.EXPORT_NAME, 'r', encoding='utf-8') as tmpFile:
+            self.data = json.load(tmpFile)
 
     def save(self):
-        """ Сохранение БД в файл """
-        with open(self.DATA_NAME, 'w', encoding='utf-8') as tmp_file:
-            tmp_file.write("[\n")
-            tmp_len = len(self.lots)
-            for tmp_i in range(tmp_len):
-                tmp_file.write(self.lots[tmp_i].__str__().replace("'", '"'))
-                if tmp_i < tmp_len - 1:
-                    tmp_file.write(",")
-                tmp_file.write("\n")
-            tmp_file.write("]")
+        """ Saving dump """
+        with open(self.EXPORT_NAME, 'w', encoding='utf-8') as tmp_file:
+            json.dump(self.data, tmp_file, ensure_ascii=False)
+        with open(self.EXPORT_NAME_JS, 'w', encoding='utf-8') as tmp_file:
+            tmp_file.write("var GData = {")
+            for tmp_item in self.data:
+                tmp_lot = self.data[tmp_item]
+                if not tmp_lot:
+                    continue
+                if not tmp_lot[1]:
+                    continue
+                tmp_file.write(str(tmp_item))
+                tmp_file.write(":['")
+                tmp_file.write(datetime.datetime.fromtimestamp(tmp_lot[0]).strftime("%H:%M:%S"))
+                tmp_file.write("',")
+                tmp_file.write(tmp_lot[1].__str__())
+                tmp_file.write(",'")
+                if len(tmp_lot) < 3:
+                    tmp_file.write(tmp_lot[0].__str__())
+                else:
+                    tmp_file.write(tmp_lot[2])
+                tmp_file.write("'],")
+            tmp_file.write("};")
+        pass
 
-    def send(self, session: VkApi, text: str, channel: int):
-        """ Отправка сообщения """
-        tmpParams = {
-            'peer_id': channel,
-            'message': text,
-            'random_id': 0
-        }
-        try:
-            session.method('messages.send', tmpParams)
-        except Exception as e:
-            print(e)
+    def run(self, bagapi: TraderApi):
+        """ Запуск жизненного цикла """
+        print("> Ready " + bagapi.bagid)
+        while True:
+            self.loadnext(bagapi)
+            time.sleep(60 / 30 * 60)
 
-    def checktrader(self):
-        """ Проверка текущего сообщения торговца """
-        if self.showall():
-            return
-        if self.setcost():
-            return
-
-    def checkowner(self):
-        """ Проверка текущего сообщения владельца """
-        if self.checktrade():
-            return
-
-    def showall(self):
-        """ Отображение списка товаров """
-        if not self.event.from_user:
-            return False
-        if self.event.user_id != self.ownerid:
-            return False
-        # Пробьем регулярку
-        tmp_match = self.reg_all.search(self.event.message)
-        if not tmp_match:
-            return False
-        # Подготовим
-        tmp_data = ""
-        for tmp_item in self.lots:
-            if tmp_item.cost > 0:
-                tmp_data += "%s: %s\n" % (tmp_item.name, tmp_item.cost)
-        # Отправим
-        self.send(self.tradersession, tmp_data, self.ownerid)
-        # Готово
-        return True
-
-    def setcost(self):
-        """ Установка цены """
-        if not self.event.from_user:
-            return False
-        if self.event.user_id != self.ownerid:
-            return False
-        # Пробьем регулярку
-        tmp_match = self.reg_set.search(self.event.message)
-        if not tmp_match:
-            return False
-        # Определим
-        tmp_slot: Optional[TradeSlot] = None
-        tmp_name = tmp_match[1]
-        tmp_cost = int(tmp_match[2])
-        # Поищем основное
-        for tmp_search in self.lots:
-            if (tmp_search.short == tmp_name) or (tmp_search.name == tmp_name):
-                tmp_slot = tmp_search
-                break
-        # Установим
-        if not tmp_slot:
-            self.send(self.tradersession, "😨%s нет в базе" % tmp_name, self.ownerid)
-            return True
-        tmp_slot.cost = tmp_cost
-        self.save()
-        self.send(self.tradersession, "👍🏻%s сохранено" % tmp_name, self.ownerid)
-        # Готово
-        return True
-
-    def checktrade(self):
-        """ Учет покупки """
-        if self.event.peer_id != self.GAME_BOT_ID:
-            return False
-        # Пробьем регулярку
-        tmp_match = self.reg_accept.search(self.event.message)
-        if not tmp_match:
-            return False
-        # Учет покупки
-        tmp_count = int(tmp_match[1])
-        tmp_name = tmp_match[2].lower()
-        print("  Куплен %s %s" % (tmp_count, tmp_name))
-        # Успешно
-        return True
-
-    def buyinc(self, pause: bool):
+    def loadinc(self):
         """ Inc current key """
         if self.lotkey == len(self.lots) - 1:
             self.lotkey = 0
         else:
             self.lotkey += 1
-        if pause:
-            time.sleep(6)
 
-    def buy(self):
-        """ Покупка """
-        tmp_lot: TradeLot
-        # Запросим с сервера
-        tmp_slot: TradeSlot = self.lots[self.lotkey]
-        if tmp_slot.code <= 0:
-            return self.buyinc(False)
-        if tmp_slot.cost <= 0:
-            return self.buyinc(False)
-        print("> query " + tmp_slot.name)
-        tmp_lots = self.traderapi.queryLots(tmp_slot.code)
-        if not tmp_lots:
-            return self.buyinc(True)
-        print("> found " + str(len(tmp_lots)))
-        # Переберем лоты
-        for tmp_lot in tmp_lots:
-            if tmp_slot.cost < tmp_lot.cost / tmp_lot.count:
-                continue
-            print("Лот %s запрос %s %s за %s" % (tmp_lot.lotnum, tmp_lot.count, tmp_slot.name, tmp_lot.cost))
-            if self.ownersession:
-                self.send(self.ownersession, "Купить лот %s" % tmp_lot.lotnum, self.GAME_BOT_ID)
-            else:
-                self.send(self.tradersession, "Купить лот %s" % tmp_lot.lotnum, self.GAME_BOT_ID)
-            return time.sleep(6)
-        # Следующий
-        self.buyinc(True)
+    def loadnext(self, bagapi: TraderApi):
+        """ Load next item """
+        self.locker.acquire()
+        try:
+            tmp_code = self.lots[self.lotkey][3]
+            tmp_name = self.lots[self.lotkey][0]
+            self.loadinc()
+        finally:
+            self.locker.release()
+        # Current damp
+        tmp_lots = bagapi.queryLots(tmp_code, tmp_name)
+        # Saving
+        self.locker.acquire()
+        try:
+            self.data[str(tmp_code)] = tmp_lots
+            self.save()
+        finally:
+            self.locker.release()
